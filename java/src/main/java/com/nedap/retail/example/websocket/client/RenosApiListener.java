409e8184289b71edc75d6003ba3dfaff56267978
Bob Hobelman <bob.hobelman@nedap.com>
2020-07-16 17:16:24 +0200

    [#172975866] 🔒 Add support for API token
    Update the java demo code so it's possible to add an api token. 
    fixit
    
    Added authentication to the HTML example. You need to supply a token and press authenticate.

--- java/src/main/java/com/nedap/retail/example/websocket/client/RenosApiListener.java
+++ java/src/main/java/com/nedap/retail/example/websocket/client/RenosApiListener.java
@@ -42,7 +42,7 @@ public class RenosApiListener implements WebSocketListener {
     private static Response parseResponse(final String message) throws MessageParsingException {
         try {
             return Optional.ofNullable(MessageParser.parseResponse(message))
-                    .filter(response -> response.response != null)
+                    .filter(response -> response.getResponse() != null)
                     .orElseThrow(() -> new MessageParsingException("response"));
         } catch (final IOException e) {
             throw new MessageParsingException("response");
@@ -51,7 +51,7 @@ public class RenosApiListener implements WebSocketListener {
 
     private static Event parseEvent(final String message) throws MessageParsingException {
         try {
-            return Optional.ofNullable(MessageParser.parseEvent(message)).filter(event -> event.type != null)
+            return Optional.ofNullable(MessageParser.parseEvent(message)).filter(event -> event.getType() != null)
                     .orElseThrow(() -> new MessageParsingException("event"));
         } catch (final IOException e) {
             throw new MessageParsingException("event");
@@ -60,54 +60,54 @@ public class RenosApiListener implements WebSocketListener {
 
     private void handleResponse(final String message) throws MessageParsingException {
         final Response response = parseResponse(message);
-        switch (response.response) {
+        switch (response.getResponse()) {
             case UNKNOWN:
-                LOG.error("There was an error with the sent request: {}", response.content.message);
+                LOG.error("There was an error with the sent request: {}", response.getContent().getMessage());
                 break;
             default:
-                LOG.info("Received response: {}", response.response);
+                LOG.info("Received response: {} {}", response.getResponse(), response.getContent().getMessage());
         }
     }
 
     private void handleEvent(final String message) throws MessageParsingException {
         final Event event = parseEvent(message);
         final StringBuilder sb = new StringBuilder();
-        sb.append("Received ").append(event.type).append(",");
+        sb.append("Received ").append(event.getType()).append(",");
         sb.append(" detected");
-        if (event.units != null) {
-            sb.append(" by unit(s) ").append(event.units);
+        if (event.getUnits() != null) {
+            sb.append(" by unit(s) ").append(event.getUnits());
         }
-        if (event.aisle != null) {
-            sb.append(" in aisle ").append(event.aisle);
+        if (event.getAisle() != null) {
+            sb.append(" in aisle ").append(event.getAisle());
         }
-        if (event.group != null) {
-            sb.append(" in group ").append(event.group);
+        if (event.getGroup() != null) {
+            sb.append(" in group ").append(event.getGroup());
         }
-        sb.append(" at ").append(event.time);
-        if (event.direction != null) {
-            sb.append(" with direction ").append(event.direction);
+        sb.append(" at ").append(event.getTime());
+        if (event.getDirection() != null) {
+            sb.append(" with direction ").append(event.getDirection());
         }
 
         List<Epc> epcs = null;
 
-        switch (event.type) {
+        switch (event.getType()) {
             case INPUT_OBSERVATION:
                 final InputObservationEvent inputEvent = (InputObservationEvent) event;
-                sb.append(" with status: ").append(inputEvent.status);
+                sb.append(" with status: ").append(inputEvent.getStatus());
                 break;
             case RFID_OBSERVATION:
                 final RfidObservationEvent rfidObservationEvent = (RfidObservationEvent) event;
                 sb.append(" with EPCs:");
-                epcs = rfidObservationEvent.epcs;
+                epcs = rfidObservationEvent.getEpcs();
                 break;
             case RFID_MOVE:
                 final RfidMoveEvent rfidMoveEvent = (RfidMoveEvent) event;
                 sb.append(" with EPCs:");
-                epcs = rfidMoveEvent.epcs;
+                epcs = rfidMoveEvent.getEpcs();
                 break;
             case SD_LABEL_DETECT:
                 final SdLabelDetectEvent sdLabelDetectEvent = (SdLabelDetectEvent) event;
-                sb.append(" SD address ").append(sdLabelDetectEvent.sdAddress);
+                sb.append(" SD address ").append(sdLabelDetectEvent.getSdAddress());
                 break;
             default:
                 // no additional information
@@ -123,21 +123,21 @@ public class RenosApiListener implements WebSocketListener {
     private void printEpcs(final List<Epc> epcs) {
         for (final Epc epc : epcs) {
             final StringBuilder sb = new StringBuilder();
-            sb.append(epc.epc);
-            if (epc.easStatus != null) {
-                sb.append(" with EAS status ").append(epc.easStatus);
+            sb.append(epc.getEpc());
+            if (epc.getEasStatus() != null) {
+                sb.append(" with EAS status ").append(epc.getEasStatus());
             }
             sb.append(" detected");
-            if (epc.units != null) {
-                sb.append(" by unit(s) ").append(epc.units);
+            if (epc.getUnits() != null) {
+                sb.append(" by unit(s) ").append(epc.getUnits());
             }
-            if (epc.aisle != null) {
-                sb.append(" in aisle ").append(epc.aisle);
+            if (epc.getAisle() != null) {
+                sb.append(" in aisle ").append(epc.getAisle());
             }
-            if (epc.group != null) {
-                sb.append(" in group ").append(epc.group);
+            if (epc.getGroup() != null) {
+                sb.append(" in group ").append(epc.getGroup());
             }
-            sb.append(" at ").append(epc.time);
+            sb.append(" at ").append(epc.getTime());
 
             LOG.info("   {}", sb.toString());
         }
